3143522a8ecc9d212ef8814bfe2f2d11b3e20e50
MhL5 <sapumr@gmail.com>
2023-09-28 17:39:49 +0330

    bug fixes

--- script.js
+++ script.js
@@ -12,24 +12,25 @@ const toggleTheme = function () {
 };
 themeCheckboxElement.addEventListener(`click`, toggleTheme);
 
-// STORE The current value
+//////////////////////////////////////////////////////////////////////////////////////
+
+// Storing current value
 let curValue = ``;
 // store the last value we had
 let lastValue = ``;
-
+// storing Last value,current value, type of the math operation
 const numStore = [];
 
 const computeNumbers = function (lastNum, curNum, type) {
   lastNum = Number(lastNum);
   curNum = Number(curNum);
-
-  console.log(type);
-  console.log(lastNum);
-  console.log(curNum);
-
+  // GUARD CLAUSE for when user clicks without a value
+  if (lastNum === 0 && curNum === 0) return;
+  // storing lastNum curNum type
   numStore.push(lastNum, curNum, type);
-  // console.log(numStore.splice(1));
-
+  console.log(numStore);
+  // if our last Value is not empty then add them together
+  // first time that function runs , last value is 0 so we need to start from second time
   if (numStore.length >= 6) {
     if (type === `=`) {
       if (numStore[numStore.length - 4] === `+`)
@@ -82,16 +83,21 @@ const computeNumbers = function (lastNum, curNum, type) {
     lastValue = lastNum;
     return;
   }
-
-  if (type === `*`) lastNum = Number(lastNum) * Number(curNum);
-  if (type === `-`) lastNum = Number(lastNum) - Number(curNum);
+  // the first function runs
+  // + - we add or decrease current number to 0
+  // / * we use 1 for the first time because the first time that function runs our last Value is 0
+  if (type === `*`) lastNum = Number(curNum) * 1;
+  if (type === `/`) lastNum = Number(curNum) / 1;
   if (type === `+`) lastNum = Number(lastNum) + Number(curNum);
-  if (type === `/`) lastNum = Number(lastNum) / Number(curNum);
-  console.log(lastNum);
+  if (type === `-`) lastNum = Number(curNum) - Number(lastNum);
+
+  // make curValue empty and store the value inside lastValue
   curValue = ``;
   lastValue = lastNum;
 };
-console.log(computeNumbers(0, 5, `/`));
+
+// console.log(computeNumbers(0, 5, `/`));
+
 const calcInit = function (e) {
   e.preventDefault();
   const targetInnerText = e.target.innerText;
@@ -101,7 +107,7 @@ const calcInit = function (e) {
   // Guard clause if its not a button return
   if (e.target.tagName !== `BUTTON`) return;
 
-  // insert the numbers inside DOM
+  // insert the numbers inside DOM as a string
   if (targetAttribute === `number`) {
     curValue += targetInnerText;
   }
@@ -113,7 +119,7 @@ const calcInit = function (e) {
     numStore.splice(0, numStore.length);
   }
 
-  // DELETE
+  // DELETE - delete last number
   if (targetAttribute === `del`) {
     curValue = curValue.slice(0, curValue.length - 1);
   }
@@ -123,10 +129,9 @@ const calcInit = function (e) {
   if (targetAttribute === `-`) computeNumbers(lastValue, curValue, `-`);
   if (targetAttribute === `+`) computeNumbers(lastValue, curValue, `+`);
   if (targetAttribute === `/`) computeNumbers(lastValue, curValue, `/`);
-  // =
   if (targetAttribute === `=`) computeNumbers(lastValue, curValue, `=`);
 
-  // render
+  // render the values inside DOM
   numOutput.innerText = curValue;
   lastComputedValue.innerText = lastValue;
 };
