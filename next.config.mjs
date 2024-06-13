131c975ebf47de82579a2b6cacff020645e229f3
Mohammad Lashani <sapumr@gmail.com>
2024-06-14 00:34:42 +0330

    add unsplash images to next js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "images.unsplash.com" }],
+  },
+};
 
 export default nextConfig;
