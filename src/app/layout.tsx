16dac9d4bbe61838da09c10450aae0a1d8e4d82b
Mohammad Lashani <sapumr@gmail.com>
2024-06-13 23:47:29 +0330

    update metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | ecommerce",
+    template: "Ecommerce | %s",
     default: "ecommerce",
   },
   description: "We make your wallet cry",
