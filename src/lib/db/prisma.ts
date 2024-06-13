e6a4fecc61358b5dcd9815f1468e59d2586bc4b5
Mohammad Lashani <sapumr@gmail.com>
2024-06-13 23:46:14 +0330

    add prisma.ts

---
+++ src/lib/db/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { env } from "../env";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
