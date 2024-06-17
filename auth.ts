52bcd18f78ee0d76226df4e76cebeb439f74383d
Mohammad Lashani <sapumr@gmail.com>
2024-06-17 12:16:39 +0330

    add and configure next auth (auth js)

---
+++ auth.ts
@@ -0,0 +1,18 @@
+import prisma from "@/lib/db/prisma";
+import { env } from "@/lib/env";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { NextAuthConfig } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import Google from "next-auth/providers/google";
+
+export const authOptions: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    Google({
+      clientId: env.AUTH_GOOGLE_ID,
+      clientSecret: env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
