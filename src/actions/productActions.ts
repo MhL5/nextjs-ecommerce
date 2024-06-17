082d6feec2da4c86c52bc7d8a1abbc3ee486ac17
Mohammad Lashani <sapumr@gmail.com>
2024-06-17 13:04:57 +0330

    update createProduct

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -2,9 +2,13 @@
 
 import prisma from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
+import { auth } from "../../auth";
 
 export async function createProduct(formData: FormData) {
   // todo: data validation ?! :|
+  const session = await auth();
+  if (!session) redirect("/api/auth/signin?callbackUrl=/add-product");
+
   const name = String(formData.get("name"));
   const description = String(formData.get("description"));
   const imageUrl = String(formData.get("imageUrl"));
