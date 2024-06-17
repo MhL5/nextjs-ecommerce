355ec89810c7a5eae13fc87e3b641afd5d17d457
Mohammad Lashani <sapumr@gmail.com>
2024-06-17 19:14:51 +0330

    add pagination

---
+++ src/components/PaginationBar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { ReactElement } from "react";
+
+type PaginationBarProps = {
+  currentPage: number;
+  totalPages: number;
+};
+
+export default function PaginationBar({
+  currentPage,
+  totalPages,
+}: PaginationBarProps) {
+  const maxPage = Math.min(totalPages, Math.max(currentPage + 4, 10));
+  const minPage = Math.max(1, Math.min(currentPage - 5, maxPage - 9));
+
+  const numberedPageItems: ReactElement[] = [];
+
+  for (let page = minPage; page <= maxPage; page++) {
+    numberedPageItems.push(
+      <Link
+        href={`?page=${page}`}
+        key={page * page}
+        className={`btn join-item ${currentPage === page ? "btn-active pointer-events-none" : ""}`}
+      >
+        {page}
+      </Link>,
+    );
+  }
+
+  return (
+    <>
+      <div className="join hidden sm:block"> {numberedPageItems} </div>
+      <div className="join block sm:hidden">
+        {currentPage > 1 && (
+          <Link href={`?page=${currentPage - 1}`} className="btn join-item">
+            ⬅️
+          </Link>
+        )}
+        <button className="btn join-item pointer-events-none">
+          Page {currentPage}
+        </button>
+        {currentPage < totalPages && (
+          <Link href={`?page=${currentPage + 1}`} className="btn join-item">
+            ➡️
+          </Link>
+        )}
+      </div>
+    </>
+  );
+}
